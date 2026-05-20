857ce3f43478e5a4eaa734b0beead694f42ab622
EL-CONS <gabrielcons117@live.com>
2026-05-20 00:16:36 -0700

    code to test categoria.php errors

--- categoria.php
+++ categoria.php
@@ -1,6 +1,10 @@
 <?php
 
-$db = new SQLite3(__DIR__ . '/../../data/postres.db');
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$db = new SQLite3('/home/a220214757/data/postres.db');
 
 $today = date('Y-m-d');
 
